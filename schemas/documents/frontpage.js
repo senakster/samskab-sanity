cbb1630d273c8d0ff4fb5cfa05f1afe93fc012cb
Emil Nakayama <senakster@gmail.com>
2022-08-29 23:15:58 +0200

    init

---
+++ schemas/documents/frontpage.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'frontpage',
+    title: 'Forside',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Titel',
+            type: 'string',
+        }
+    ]
+}
